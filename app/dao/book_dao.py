5401e41619db9f22825952bda463a7a46b119132
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-09-18 19:43:36 -0300

    Adicionando banco de livros

--- app/dao/book_dao.py
+++ app/dao/book_dao.py
@@ -1,5 +1,7 @@
 from app.helpers.decorators import is_not_null
 from app.model.book import Book
+from app.model.author import Author
+from app.model.category import Category
 
 from typing import List
 
@@ -7,36 +9,89 @@ from typing import List
 class BookDao:
     """Banco de Dados de Livros"""
 
-    list_book: List[Book] = []
+    def __init__(self, connection):
+        self._connection = connection
+        self._cursor = self._connection.cursor(dictionary=True, buffered=True)
 
     def save(self, book: Book) -> None:
         if not isinstance(book, Book):
             raise TypeError('O argumento passado não é do tipo Livro')
 
-        if book in self.list_book:
-            raise ValueError('Este título já consta em nossos acervos.')
+        sql_category_id = 'SELECT id FROM category WHERE name = %s'
+        self._cursor.execute(sql_category_id, (book.category_name,))
+        category_id = self._cursor.fetchone()
 
-        self.list_book.append(book)
+        sql_author_id = 'SELECT id FROM author WHERE name = %s'
+        self._cursor.execute(sql_author_id, (book.author_name,))
+        author_id = self._cursor.fetchone()
 
-        print(f'\n---Livro Cadastrado---\n{book}')
+        sql = """INSERT INTO book(
+                category_id, author_id, title, 
+                resume, summary, number_of_pages,
+                isbn,edition, price) VALUES 
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        args = (
+            category_id['id'], author_id['id'], book.title,
+            book.resume, book.summary, book.number_of_pages,
+            book.isbn, book.edition, book.price
+        )
+
+        self._cursor.execute(sql, args)
+        self._connection.commit()
+
+        print(f'\n---Livro inserido no banco---\n{book}')
 
     @is_not_null
     def find_many(self, title: str) -> List[Book]:
+
+        list_book = []
+
         if len(title) < 2:
-            raise ValueError(
-                'É necessário ao menos 2 caracteres para fazer a busca')
+            raise ValueError('É necessário ao menos 2 caracteres para fazer a busca')
 
-        books_with_title = list(
-            filter(lambda books: title.lower() in books.title.lower(),  self.list_book))
+        sql = 'SELECT * FROM book WHERE title LIKE %s'
+        self._cursor.execute(sql, (f'%{title}%', ))
+        result_query_book = self._cursor.fetchall()
 
-        if not books_with_title:
+        if not result_query_book:
             raise KeyError('Este título não consta no acervo')
 
-        return books_with_title
+        for item in result_query_book:
+            book = Book(item['title'], item['resume'], item['summary'], item['number_of_pages'],
+                        item['isbn'], self.__search_author_for_id(item['author_id']),
+                        self.__search_category_for_id(item['category_id']),
+                        item['edition'], item['price'])
+
+            list_book.append(book)
+
+        return list_book
 
     @is_not_null
     def find_one(self, title: str) -> Book:
-        for book in self.list_book:
-            if title.lower() == book.title.lower():
-                return book
-        raise KeyError('Este título não consta no acervo')
+        sql = 'SELECT * FROM book WHERE title = %s'
+        self._cursor.execute(sql, (title, ))
+        result_query = self._cursor.fetchone()
+
+        if not result_query:
+            raise KeyError('Este título não consta no acervo')
+
+        author = self.__search_author_for_id(result_query['author_id'])
+        category = self.__search_category_for_id(result_query['category_id'])
+        
+        return Book(result_query['title'], result_query['resume'], result_query['summary'],
+                    result_query['number_of_pages'], result_query['isbn'], author, category,
+                    result_query['edition'], result_query['price'])
+
+    def __search_category_for_id(self, id) -> Category:
+        sql_category = 'SELECT * FROM category WHERE id = %s'
+        self._cursor.execute(sql_category, (id,))
+        category = self._cursor.fetchone()
+
+        return Category(category['name'])
+
+    def __search_author_for_id(self, id) -> Author:
+        sql_author = 'SELECT * FROM author WHERE id = %s'
+        self._cursor.execute(sql_author, (id,))
+        author = self._cursor.fetchone()
+
+        return Author(author['name'], author['email'])
