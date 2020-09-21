aa4e478d514b71b0f580c8cad94a7f1ef97a7af6
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-09-21 10:47:08 -0300

    Refatorando teste do banco de categorias

--- app/tests/test_dao/test_category.py
+++ app/tests/test_dao/test_category.py
@@ -1,40 +1,56 @@
 from app.model.category import Category
 from app.dao.category_dao import CategoryDao
+from app.config.connection_database import ConnectionDatabase
+import mysql.connector.errors as db
 
 import unittest
 
 class TestCategoryDatabase(unittest.TestCase):
 
+    def setUp(self):
+        self.connection = ConnectionDatabase.connect()
+        self.cursor = self.connection.cursor(dictionary=True)
+
+    def tearDown(self):
+        sql_delete = 'DELETE FROM category WHERE name = "Programming" OR name="Data Science"'
+        self.cursor.execute(sql_delete)
+        self.connection.commit()
+        self.connection.close()
+
     def test_should_throw_an_exception_when_add_name_of_category_already_exists(self):
-        dao = CategoryDao()
+        
+        dao = CategoryDao(self.connection)
         category_programming = Category('Programming')
         category_already_exists_programming = Category('Programming')
         dao.save(category_programming)
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(db.IntegrityError):
             dao.save(category_already_exists_programming)
     
-    def tearDown(self):
-        CategoryDao.list_categories = []
 
     def test_should_throw_an_exception_when_save_other_type_different_of_category(self):
-        dao = CategoryDao()
+        dao = CategoryDao(self.connection)
         type_str = 'Type str'
         
         with self.assertRaises(TypeError):
             dao.save(type_str)
 
     def test_should_throw_an_exception_when_find_by_book_not_exists(self):
-        dao = CategoryDao()
-        with self.assertRaises(KeyError):
-            dao.find_one('Ciências')
+        dao = CategoryDao(self.connection)
 
-    def test_should_add_category_in_list_categories_of_database(self):
-        dao = CategoryDao()
+        with self.assertRaises(KeyError):
+            dao.find_one('Ciências Avançadas')
+
+    def test_should_add_category_in_database(self):
+        dao = CategoryDao(self.connection)
         category_data_science = Category('Data Science')
         dao.save(category_data_science)
+
+        sql = 'SELECT name FROM category WHERE name="Data Science"'
+        self.cursor.execute(sql)
+        result_query = self.cursor.fetchone()
         
-        assert category_data_science in dao.list_categories, "Temos um problema. A categoria não foi salva no database"
+        self.assertEqual('Data Science', result_query['name'])
 
 if __name__ == '__main__':
     unittest.main()
