c3824de41642cbe88487558c205dfc90ddfd367a
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-09-21 10:47:42 -0300

    Refatorando testes do banco de autor

--- app/tests/test_dao/test_author.py
+++ app/tests/test_dao/test_author.py
@@ -1,35 +1,49 @@
 from app.model.author import Author
 from app.dao.author_dao import AuthorDao
+from app.config.connection_database import ConnectionDatabase
+import mysql.connector.errors as db
 
 import unittest
 
 class TestAuthorDatabase(unittest.TestCase):
 
+    def setUp(self):
+        self.connection = ConnectionDatabase.connect()
+        self.cursor = self.connection.cursor(dictionary=True)
+
+
     def _setup(self, name='Luciano Ramalho', email='luciano@luciano.com.br'):
         return Author(name, email)
 
     def tearDown(self):
-        AuthorDao.list_authors = []
+        sql_delete = 'DELETE FROM author WHERE email = "luciano@luciano.com.br"'
+        self.cursor.execute(sql_delete)
+        self.connection.commit()
+        self.connection.close()
     
     def test_should_throw_an_exception_when_add_email_of_author_already_exists(self):
-        dao = AuthorDao()
-        with self.assertRaises(ValueError):
+        dao = AuthorDao(self.connection)
+        with self.assertRaises(db.IntegrityError):
             dao.save(self._setup())
             dao.save(self._setup())
 
     def test_should_throw_an_exception_when_save_other_type_different_of_author(self):
-        dao = AuthorDao()
+        dao = AuthorDao(self.connection)
         type_str = 'Type str'
         with self.assertRaises(TypeError):
             dao.save(type_str)
     
-    def test_should_add_author_in_list_authors_of_database(self):
-        dao = AuthorDao()
+    def test_should_add_author_in_database(self):
+        dao = AuthorDao(self.connection)
         author_luciano = self._setup()
         dao.save(author_luciano)
-
-        assert author_luciano in dao.list_authors, "Temos um problema. O autor não foi salvo no database"
         
+        sql = 'SELECT email FROM author WHERE email="luciano@luciano.com.br"'
+        self.cursor.execute(sql)
+        result_query = self.cursor.fetchone()
+        
+        self.assertEqual('luciano@luciano.com.br', result_query['email'])
+
 
 if __name__ == '__main__':
     unittest.main()
