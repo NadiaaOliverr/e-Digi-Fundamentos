3d3d40dfcaa3da025278a6c0f0a5d46c55bad772
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-10-01 12:46:07 -0300

    Adicionando JOIN em find_one e find_many

--- app/dao/book_dao.py
+++ app/dao/book_dao.py
@@ -50,7 +50,12 @@ class BookDao:
         if len(title) < 2:
             raise ValueError('É necessário ao menos 2 caracteres para fazer a busca')
 
-        sql = 'SELECT * FROM book WHERE title LIKE %s'
+        sql = """
+                SELECT title, resume, summary, number_of_pages, isbn, edition, price, c.name as 'name_category', a.name as 'name_author', a.email 
+                FROM book as b join category as c join author as a on b.category_id=c.id and b.author_id = a.id 
+                WHERE title LIKE %s
+            """
+        
         self._cursor.execute(sql, (f'%{title}%', ))
         result_query_book = self._cursor.fetchall()
 
@@ -59,8 +64,8 @@ class BookDao:
 
         for item in result_query_book:
             book = Book(item['title'], item['resume'], item['summary'], item['number_of_pages'],
-                        item['isbn'], self.__search_author_for_id(item['author_id']),
-                        self.__search_category_for_id(item['category_id']),
+                        item['isbn'], Author(item['name_author'], item['email']),
+                        Category(item['name_category']),
                         item['edition'], item['price'])
 
             list_book.append(book)
@@ -69,30 +74,17 @@ class BookDao:
 
     @is_not_null
     def find_one(self, title: str) -> Book:
-        sql = 'SELECT * FROM book WHERE title = %s'
+        sql = """
+                SELECT title, resume, summary, number_of_pages, isbn, edition, price, c.name as 'name_category', a.name as 'name_author', a.email 
+                FROM book as b join category as c join author as a on b.category_id=c.id and b.author_id = a.id 
+                WHERE title = %s
+            """
         self._cursor.execute(sql, (title, ))
         result_query = self._cursor.fetchone()
 
         if not result_query:
             raise KeyError('Este título não consta no acervo')
-
-        author = self.__search_author_for_id(result_query['author_id'])
-        category = self.__search_category_for_id(result_query['category_id'])
         
         return Book(result_query['title'], result_query['resume'], result_query['summary'],
-                    result_query['number_of_pages'], result_query['isbn'], author, category,
-                    result_query['edition'], result_query['price'])
-
-    def __search_category_for_id(self, id) -> Category:
-        sql_category = 'SELECT * FROM category WHERE id = %s'
-        self._cursor.execute(sql_category, (id,))
-        category = self._cursor.fetchone()
-
-        return Category(category['name'])
-
-    def __search_author_for_id(self, id) -> Author:
-        sql_author = 'SELECT * FROM author WHERE id = %s'
-        self._cursor.execute(sql_author, (id,))
-        author = self._cursor.fetchone()
-
-        return Author(author['name'], author['email'])
+                    result_query['number_of_pages'], result_query['isbn'], Author(result_query['name_category'], result_query['email']),
+                    Category(result_query['name_category']), result_query['edition'], result_query['price'])
